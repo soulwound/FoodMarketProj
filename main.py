f3b5f2a5699f54ab03faedfd291639339e8b3b92
Victor <guyswinter0@gmail.com>
2023-05-02 20:19:42 +0200

    add page for error 404

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def index():
     return render_template('base.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
