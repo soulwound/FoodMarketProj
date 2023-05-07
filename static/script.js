ba5bb4d9bd5e05a7b4eeec752a97149e5579cf45
Victor <guyswinter0@gmail.com>
2023-05-07 12:30:15 +0300

    cart updated

--- static/script.js
+++ static/script.js
@@ -5,3 +5,27 @@ function show() {
         document.querySelector('#cart').style.display = 'none'
     }
 }
+
+let cart_list = {
+    'roll-fila': 0,
+    'sushi-shrimp': 0,
+};
+
+function addToCart(id) {
+    cart_list[id]++;
+    console.log(cart_list);
+    document.querySelector("#"+id).setAttribute('disabled', '');
+    document.querySelector("#"+id).textContent = 'В корзине';
+    document.querySelector("#"+id).style.backgroundColor = '#C95C3F'
+    updateCart(cart_list);
+}
+
+function updateCart(cart_list) {
+    if (cart_list == 0) {
+        document.querySelector('#if-cart-is-empty-img').style.display = 'block';
+        document.querySelector('#if-cart-is-empty-alert').style.display = 'block';
+    } else {
+        document.querySelector('#if-cart-is-empty-img').style.display = 'none';
+        document.querySelector('#if-cart-is-empty-alert').style.display = 'none';
+    }
+}
