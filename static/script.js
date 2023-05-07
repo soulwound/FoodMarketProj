2761993ee4ef2193aaeecdac14853ec8b6caaffd
Victor <guyswinter0@gmail.com>
2023-05-07 10:24:36 +0200

    cart button now have func

---
+++ static/script.js
@@ -0,0 +1,7 @@
+function show() {
+    if (document.querySelector('#cart').style.display == 'none') {
+        document.querySelector('#cart').style.display = 'block'
+    } else {
+        document.querySelector('#cart').style.display = 'none'
+    }
+}
